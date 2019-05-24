a07df6bab3e35efc152fd24c33aabdecffdb6ab4
yvan <y.douenel@coopernet.fr>
2019-05-24 14:42:36 +0200

    img_from_crealead.js up.

--- web/themes/custom/nj/less/js/img_from_crealead.js
+++ web/themes/custom/nj/less/js/img_from_crealead.js
@@ -1,14 +1,16 @@
 jQuery(function ($) {
-    console.log("hello boy");
-    
-    $(".page-node-type-article .field--name-field-image-depuis-crealead .field--item").each(function(){
+    console.log("dans img_from crealead");
+
+    $(".field--name-field-image-depuis-crealead .field--item").each(function(){
+      console.log("Dans premier each");
         $("<img />",{
             src: $(this).text()
         }).insertAfter($(this));
         $(this).hide();
     });
-    $(".path-frontpage .field--name-field-image-depuis-crealead, .path-articles .field--name-field-image-depuis-crealead").each(function(){
-        $("<img />",{
+    $(".path-frontpage .field--name-field-image-depuis-crealead, .field--name-field-image-depuis-crealead").each(function(){
+      console.log("Dans second each");
+      $("<img />",{
             src: $(this).text()
         }).insertAfter($(this));
         $(this).hide();
